351d523283af60889c01bf9d20b03f125af8fd57
Dallton96 <88783947+Dallton96@users.noreply.github.com>
2024-05-19 15:50:44 -0500

    Update productCards.js

--- scripts/productCards.js
+++ scripts/productCards.js
@@ -1,7 +1,7 @@
 export function CreateCard(product) {
   return `
       <article class="product-card">
-      <a href="/details.html?id=${product.id}">
+      <a href="/store_Sinertron/details.html?id=${product.id}">
          <img class="product-imagen" src="${product.images[0]}" alt="${product.id}">
          <div class="info">
             <span class="titulo_articulo">${product.title}</span>
